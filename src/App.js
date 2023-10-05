8039c2bb33105564f0d0d4ee99ff537182004e8d
wilfried <wilfriedpapt@gmail.com>
2023-10-05 23:34:06 +0200

    adding fetch method to fetch data from spring boot backend

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fectMoviesHandler = () => {
+    fetch("http://localhost:8080/films/1")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = [
+          {
+            id: data.id,
+            title: data.title,
+            openingText: data.openingText,
+            releaseDate: data.releaseDate,
+          },
+        ];
+        setMovies(transformedMovies);
+      });
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fectMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
